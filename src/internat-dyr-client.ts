48a1679b93ca98826843182264946625eb65927c
KristofferK <kristoffer@minlejebolig.dk>
2018-03-08 22:15:37 +0100

    Add functionality to load more rows

--- src/internat-dyr-client.ts
+++ src/internat-dyr-client.ts
@@ -14,6 +14,20 @@ export class InternatDyrClient {
     return Promise.resolve(new InternatDyrClient(browser, page));
   }
 
+  async acceptCookies() {
+    await this.page.evaluate(() => {
+      const cookieButton = <HTMLButtonElement>document.querySelector('.cookie.alert.alert-info button');
+      if (cookieButton != null) {
+        cookieButton.click();
+      }
+
+      const cookieDiv = <HTMLDivElement>document.querySelector('.cookie-wrap');
+      if (cookieDiv != null) {
+        cookieDiv.remove();
+      }
+    });
+  }
+
   async search(zip: number, species: Animal[], distance: Distance) {
     await this.page.goto("https://internat-dyr.dk/index/");
 
@@ -33,6 +47,19 @@ export class InternatDyrClient {
 
     await this.page.click('#search-zip button[type="submit"]');
     await this.page.waitForNavigation();
+  }
+
+  async loadMoreResults(timeInMillisecondsToWaitForResponse: number = 1000) {
+    const rowsBeforeLoadingMore = await this.page.evaluate(() => document.querySelectorAll('#animallist .row').length);
+    await this.page.evaluate(() => {
+      const loadMoreButton = <HTMLButtonElement>document.querySelector('#load-more');
+      if (loadMoreButton != null) {
+        loadMoreButton.click();
+      }
+    });
+    await this.page.waitFor(timeInMillisecondsToWaitForResponse);
+    const rowsAfterLoadingMore = await this.page.evaluate(() => document.querySelectorAll('#animallist .row').length);
+    return rowsAfterLoadingMore - rowsBeforeLoadingMore;
   }
 
   async screenshot(path: string, fullPage: boolean) {
--- src/script.ts
+++ src/script.ts
@@ -3,7 +3,12 @@ import { Animal } from "./animal";
 import { Distance } from "./distance";
 
 (async() => {
-    const client = await InternatDyrClient.initialize();
-    await client.search(5000, [Animal.Cats, Animal.Rabbits, Animal.Reptiles], Distance.Close);
-    await client.screenshot('dump.jpg', true);
+  const client = await InternatDyrClient.initialize();
+  await client.acceptCookies();
+  await client.search(8000, [Animal.Cats, Animal.Rabbits, Animal.Reptiles], Distance.Close);
+  await client.screenshot('dump.jpg', true);
+  for (let i = 0; i < 4; i++) {
+    console.log('loaded', await client.loadMoreResults(), 'more rows');
+  }
+  await client.screenshot('dump2.jpg', true);
 })();
